00ab5cea876c71745da1519eaad967bf1800a803
Thiago Reis <reist36@gmail.com>
2023-12-14 20:07:31 -0300

    Create: First class - Creating Position Class.

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,14 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
+		Position p1 = new Position(3, 5);
 		
+		System.out.println(p1);
 
 	}
 
